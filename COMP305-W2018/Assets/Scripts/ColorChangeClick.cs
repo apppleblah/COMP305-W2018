4baf93c948d904aa67e146a19bb985dae38b2f89
Jeffrey <appplebla@gmail.com>
2018-02-04 16:37:03 -0500

    Lab2

---
+++ COMP305-W2018/Assets/Scripts/ColorChangeClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangeClick : MonoBehaviour
+{
+
+    public GameObject player;
+    public Sprite newSprite;
+
+    private SpriteRenderer sRend;
+
+	// Use this for initialization
+	void Start ()
+    {
+        sRend = player.GetComponent<SpriteRenderer>();
+	}
+
+    void OnMouseDown()
+    {
+        sRend.sprite = newSprite;
+
+    }
+	
+
+}
